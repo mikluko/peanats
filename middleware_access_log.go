22f5db01bd8f53946b90f8e54ef4f87193303d01
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2024-02-02 09:39:09 +0100

    Rework access log middleware

--- middleware_access_log.go
+++ middleware_access_log.go
@@ -3,13 +3,18 @@ package peanats
 import (
 	"io"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/nats-io/nuid"
 )
 
-func MakeAccessLogMiddleware(w io.Writer) Middleware {
-	logger := log.New(w, "", log.Ldate|log.Ltime|log.LUTC)
+// MakeAccessLogMiddleware returns a middleware that logs the request and response of a handler.
+//
+// WARNING: the logger is inefficient and should not be used in production.
+// TODO: replace with efficient implementation.
+func MakeAccessLogMiddleware(opts ...AccessLogMiddlewareOption) Middleware {
 	return func(next Handler) Handler {
 		return HandlerFunc(func(pub Publisher, req Request) error {
 			uid := req.Header().Get(HeaderRequestUID)
@@ -18,37 +23,65 @@ func MakeAccessLogMiddleware(w io.Writer) Middleware {
 			}
 			p := loggingPublisher{
 				Publisher: pub,
-				log:       logger.Println,
-				uid:       uid,
-				subj:      req.Subject(),
 				started:   time.Now(),
+				subj:      req.Subject(),
+				uid:       uid,
+			}
+			for i := range opts {
+				opts[i](&p)
+			}
+			if p.log == nil {
+				AccessLogMiddlewareWithWriter(log.Writer())(&p)
 			}
 			err := next.Serve(&p, req)
-			if !p.hasPublished && !p.hasAcked {
-				logger.Println(p.uid, p.subj, time.Since(p.started).Milliseconds(), "noop")
+			if !p.hasPublished {
+				p.log.Println(p.uid, p.subj, time.Since(p.started).Milliseconds(), "noop")
 			}
 			return err
 		})
 	}
 }
 
+type AccessLogger interface {
+	Println(v ...any)
+}
+
+type AccessLogMiddlewareOption func(*loggingPublisher)
+
+func AccessLogMiddlewareWithLogger(log AccessLogger) AccessLogMiddlewareOption {
+	return func(p *loggingPublisher) {
+		p.log = log
+	}
+}
+
+func AccessLogMiddlewareWithWriter(w io.Writer) AccessLogMiddlewareOption {
+	return AccessLogMiddlewareWithLogger(log.New(w, "", log.Ldate|log.Ltime|log.LUTC))
+}
+
 type loggingPublisher struct {
 	Publisher
-	log          func(v ...any)
+	log          AccessLogger
 	uid          string
 	subj         string
 	started      time.Time
-	hasAcked     bool
 	hasPublished bool
 }
 
 func (p *loggingPublisher) Publish(data []byte) error {
+	p.hasPublished = true
 	err := p.Publisher.Publish(data)
 	if err != nil {
 		return err
 	}
-	p.hasPublished = true
-	p.log(p.uid, p.subj, time.Since(p.started).Milliseconds(), "pub", len(data), p.errCode(), p.errMsg())
+	bits := []string{p.uid, p.subj,
+		strconv.Itoa(int(time.Since(p.started).Milliseconds())),
+		"pub",
+		p.Subject(),
+		strconv.Itoa(len(data)),
+		p.errCode(),
+		p.errMsg(),
+	}
+	p.log.Println(strings.TrimSpace(strings.Join(bits, " ")))
 	return nil
 }
 
