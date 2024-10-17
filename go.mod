285539d3fa10b5dbc6002bbdf26fd6f6a7bd6eb9
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2024-10-17 16:45:02 +0200

    Add Raft package

--- go.mod
+++ go.mod
@@ -4,28 +4,32 @@ go 1.22
 
 require (
 	github.com/alitto/pond v1.8.3
-	github.com/nats-io/nats-server/v2 v2.10.9
-	github.com/nats-io/nats.go v1.32.0
+	github.com/nats-io/graft v0.0.0-20240402192949-3f8f888dc8af
+	github.com/nats-io/nats-server/v2 v2.10.12
+	github.com/nats-io/nats.go v1.34.0
 	github.com/nats-io/nuid v1.0.1
+	github.com/samber/slog-multi v1.2.3
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/multierr v1.11.0
-	golang.org/x/sync v0.6.0
+	golang.org/x/sync v0.8.0
 	google.golang.org/protobuf v1.33.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/klauspost/compress v1.17.5 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/minio/highwayhash v1.0.2 // indirect
-	github.com/nats-io/jwt/v2 v2.5.3 // indirect
+	github.com/nats-io/jwt/v2 v2.5.5 // indirect
 	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/samber/lo v1.47.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ raft/raft_test.go
@@ -0,0 +1,121 @@
+//go:build acc
+
+package raft_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"path"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	slogmulti "github.com/samber/slog-multi"
+	"golang.org/x/sync/semaphore"
+
+	"github.com/mikluko/peanats/raft"
+)
+
+// Example demonstrates how to use the raft package. Requires running NATS server on localhost:4222.
+//
+// Running with below command will produce verbose output on each run, disregarding the cache.
+//
+//	go test -count=1 -tags=acc -v ./raft
+//
+// To also watch Raft negotiation on NATS run:
+//
+//	nats subscribe 'graft.>'
+//
+// Happy rafting.
+func Example() {
+	slog.SetDefault(slog.New(slogmulti.Fanout(
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}),
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				if a.Key == slog.TimeKey && len(groups) == 0 {
+					return slog.Attr{}
+				}
+				return a
+			},
+		}),
+	)))
+
+	sem := semaphore.NewWeighted(3)
+	for i := 0; i < 5; i++ {
+		_ = sem.Acquire(context.Background(), 1)
+		go func() {
+			defer sem.Release(1)
+			state, err := worker(i)
+			if err != nil && !errors.Is(err, context.DeadlineExceeded) {
+				slog.Error("errored", slog.Int("worker", i), slog.Any("err", err))
+			} else {
+				slog.Info("completed", slog.Int("worker", i), slog.String("state", state.String()))
+			}
+		}()
+		time.Sleep(time.Second * 4)
+	}
+
+	_ = sem.Acquire(context.Background(), 3)
+	// Output:
+	// level=INFO msg=completed worker=0 state=Leader
+	// level=INFO msg=completed worker=1 state=Leader
+	// level=INFO msg=completed worker=2 state=Leader
+	// level=INFO msg=completed worker=3 state=Leader
+	// level=INFO msg=completed worker=4 state=Candidate
+}
+
+func worker(idx int) (raft.State, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log := slog.With("worker", idx)
+	log.Debug("joining")
+	defer log.Debug("leaving")
+
+	nc, err := nats.Connect("nats://localhost:4222")
+	if err != nil {
+		panic(err)
+	}
+	defer nc.Close()
+
+	r, err := raft.NewRaft(nc,
+		raft.WithSize(3),
+		raft.WithContext(ctx),
+		raft.WithLog(fmt.Sprintf(path.Join(os.TempDir(), "raft.%d.log"), idx)),
+		raft.WithStateChangeHandler(func(from raft.State, to raft.State) error {
+			log.Log(ctx, slog.LevelDebug-1, "state change", "from", from, "to", to)
+			return nil
+		}),
+	)
+	if err != nil {
+		return raft.Follower, err
+	}
+
+	var state raft.State
+
+	state = r.State()
+	log.Debug("reached", slog.String("state", state.String()))
+
+	err = r.Wait(ctx, raft.Candidate)
+	if err != nil {
+		return state, err
+	}
+
+	state = r.State()
+	log.Debug("reached", slog.String("state", state.String()))
+
+	err = r.Wait(ctx, raft.Leader)
+	if err != nil {
+		return state, err
+	}
+
+	state = r.State()
+	log.Debug("reached", slog.String("state", state.String()))
+
+	return state, nil
+}
